348dd056ba2159f8b23a1508194c216e9712f97a
harisssssss <haris.dervisevic@elev.ga.ntig.se>
2020-12-08 10:39:20 +0100

    start

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const dBModule = require('./dBModule')
+const dbmodule = require('./dbmodule')
 const personModel = require('./PersonModel')
 const app = express()
 const port = 3000
@@ -10,7 +10,32 @@ app.use(express.json())
 app.use(express.urlencoded())
 app.use(express.static(clientDir))
 
+app.set('view engine', 'ejs')
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
+
+app.get('/', (req, res) => res.sendFile(__dirname + "\\client\\homePage.html"))
+
+app.get('/signUp',function(req,res){
+  res.render('signUp.ejs', { });
+ });
+
+
+app.post('/', (req, res) => {
+  let person = personModel.createPerson({ name: req.body.name, email: req.body.email});
+  
+  dBModule.store(person)
+  person.save();
+  res.redirect();
+})
+
+const logger = function (req, res, next) {
+  console.log('logging')
+  next()
+}
+
+app.use(logger)
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 }) 
